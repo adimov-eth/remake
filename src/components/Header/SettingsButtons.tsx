009e5d4f3bc84d68e9c73dddee7aa11ffa71ed51
Boris Adimov <boris@adimov.ru>
2024-09-04 16:09:50 +0300

    fix show stories

--- src/components/Header/SettingsButtons.tsx
+++ src/components/Header/SettingsButtons.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { styled } from '@stitches/react'
-import { Link } from "@/components/Link"
-import InfoIcon from '@/assets/info.svg?react'
-import SettingsIcon from "@/assets/settings.svg?react"
+import React from 'react';
+import { styled } from '@stitches/react';
+import { Link } from '@/components/Link';
+import InfoIcon from '@/assets/info.svg?react';
+import SettingsIcon from '@/assets/settings.svg?react';
+import { $isNew, $storieIndex } from '@/stores/state';
 
 const ButtonsContainer = styled('div', {
   display: 'flex',
   alignItems: 'center',
   gap: '4px',
-})
+});
 
 const ButtonWrapper = styled('div', {
   display: 'flex',
@@ -21,7 +22,7 @@ const ButtonWrapper = styled('div', {
   backdropFilter: 'blur(24px)',
   WebkitBackdropFilter: 'blur(24px)',
   cursor: 'pointer',
-})
+});
 
 const Icon = styled('svg', {
   width: '24px',
@@ -29,19 +30,22 @@ const Icon = styled('svg', {
   '& path': {
     fill: '#fff',
   },
-})
+});
+
+const handleInfoClick = () => {
+  $isNew.set(true);
+  $storieIndex.set(0);
+};
 
 export const SettingsButtons: React.FC = () => (
   <ButtonsContainer>
-    <Link to="/onboarding">
-      <ButtonWrapper > 
-        <Icon as={InfoIcon} />
-      </ButtonWrapper>
-    </Link>
+    <ButtonWrapper onClick={handleInfoClick}>
+      <Icon as={InfoIcon} />
+    </ButtonWrapper>
     <Link to="/settings">
       <ButtonWrapper>
         <Icon as={SettingsIcon} />
       </ButtonWrapper>
     </Link>
   </ButtonsContainer>
-)
+);
