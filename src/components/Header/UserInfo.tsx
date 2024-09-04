64ab2b4ffd7d2fefd8e2e31f8600818bb058892c
Boris Adimov <boris@adimov.ru>
2024-09-04 18:58:34 +0300

    fix friends illustration

--- src/components/Header/UserInfo.tsx
+++ src/components/Header/UserInfo.tsx
@@ -1,41 +1,40 @@
-import React from 'react'
-import { styled } from '@stitches/react'
-import { Link } from "@/components/Link"
-import { Avatar } from '@/components/Avatar'
-import { AutoSizeText } from './AutoSizeText'
-import { User } from '@telegram-apps/sdk-react'
+import React from 'react';
+import { styled } from '@stitches/react';
+import { Link } from '@/components/Link';
+import { Avatar } from '@/components/Avatar';
+import { AutoSizeText } from './AutoSizeText';
+import { User } from '@telegram-apps/sdk-react';
 
 const UserLink = styled(Link, {
   display: 'flex',
   alignItems: 'center',
   gap: '10px',
-})
+});
 
 const UserInfoWrapper = styled('div', {
   display: 'flex',
   flexDirection: 'column',
-  gap: '7px',
-})
+  gap: '4px',
+});
 
 const UserName = styled(AutoSizeText, {
   fontFamily: 'var(--font-pro)',
   fontSize: '12px',
   fontWeight: 590,
   color: 'white',
-})
+});
 
 const UserRank = styled('div', {
   fontFamily: 'var(--font-pro)',
   fontSize: '12px',
   fontWeight: 590,
   color: '#3B4660',
-})
-
+});
 
 interface UserInfoProps {
-  user: User | null | undefined
-  rank: string
-  avatar: string | undefined
+  user: User | null | undefined;
+  rank: string;
+  avatar: string | undefined;
 }
 
 export const UserInfo: React.FC<UserInfoProps> = ({ user, rank, avatar }) => (
@@ -43,11 +42,9 @@ export const UserInfo: React.FC<UserInfoProps> = ({ user, rank, avatar }) => (
     <Avatar src={avatar} size={40} />
     <UserInfoWrapper>
       <UserName initialFontSize={14}>
-        {user
-          ? user.username || `${user.firstName} ${user.lastName}`
-          : 'Username'}
+        {user ? user.username || `${user.firstName} ${user.lastName}` : 'Username'}
       </UserName>
       <UserRank>{rank}</UserRank>
     </UserInfoWrapper>
   </UserLink>
-)
+);
