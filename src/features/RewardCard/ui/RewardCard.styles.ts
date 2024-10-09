be909eb0f4caa1fa8998eec40dcb53a9be557ac0
Vladislav Bulanov <vi3ull@icloud.com>
2024-10-09 13:58:44 +0300

    fix reward cards

--- src/features/RewardCard/ui/RewardCard.styles.ts
+++ src/features/RewardCard/ui/RewardCard.styles.ts
@@ -2,68 +2,55 @@ import { styled } from '@app/stitches.config';
 import bg from '@shared/assets/reward-bg.svg';
 
 export const Root = styled('button', {
+  '--root-bg': 'rgba(20, 21, 30, 1)',
+  '--content-bg': 'rgba(20, 21, 30, 1)',
+  '--before-bg': 'none',
+  '--before-mask': 'none',
+  '--rounded': '1rem',
+
   position: 'relative',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  padding: '1rem',
-  borderRadius: '1rem',
-  background: 'rgba(20, 21, 30, 1)',
+  padding: '0.1875rem',
   textAlign: 'center',
   fontFamily: 'var(--font-mono)',
   fontSize: '0.875rem',
   fontWeight: '600',
+  lineHeight: '1',
   color: '#fff',
   border: 'none',
-  height: '6.75rem',
-  '&:disabled': {
-    opacity: 0.4,
-    cursor: 'not-allowed',
-  },
+  borderRadius: 'var(--rounded)',
+  background: 'var(--root-bg)',
   '&:last-child': {
-    background: 'radial-gradient(64.81% 64.81% at 50% 50%, #121e45 0%, #5634cb 100%)',
+    '--root-bg': 'radial-gradient(64.81% 64.81% at 50% 50%, #121e45 0%, #5634cb 100%)',
+    '--content-bg': `url(${bg})`,
     boxShadow: '0px 0px 8px 0px #473bbb',
-    '&:before': {
-      content: '',
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      borderRadius: '1rem',
-      backgroundImage: `url(${bg})`,
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-      pointerEvents: 'none',
-    },
   },
   variants: {
-    progressStatus: {
-      claimed_reward: {
-        opacity: 0.4,
-      },
-      complete: {
-        position: 'relative',
-        background: '#14151e',
-        '&::before': {
-          content: '',
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          borderRadius: '1rem',
-          padding: '0.1875rem',
-          background: 'linear-gradient(88.72deg, #264dd0 0%, #5931ae 102.05%)',
-          '-webkit-mask': 'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)',
-          mask: 'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)',
-          '-webkit-mask-composite': 'xor',
-          'mask-composite': 'exclude',
-        },
+    complete: {
+      true: {
+        '--root-bg': 'linear-gradient(88.72deg, #264dd0 0%, #5931ae 102.05%)',
+      }
+    },
+    disabled: {
+      true: {
+        opacity: '0.4',
+        cursor: 'not-allowed',
       },
     },
   },
+});
+
+export const Content = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  padding: '1rem 0.5rem',
+  borderRadius: 'var(--rounded)',
+  background: 'var(--content-bg)',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'center, cover',
+  backgroundPosition: 'center, center',
+  height: '6.75rem',
 });
 
 export const Value = styled('div', {
