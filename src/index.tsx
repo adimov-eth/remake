0a5be76a55a4093c38a237296d09c8e37bd9bcb5
Pavel Listopad <samp1etex7@gmail.com>
2024-10-14 20:19:02 +0700

    Add base sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import { postEvent } from '@telegram-apps/sdk-react';
+import * as Sentry from "@sentry/react";
 
 import { App } from '@app/App.tsx';
+
 
 // Uncomment this import in case, you would like to develop the application even outside
 // the Telegram application, just in your browser.
@@ -11,5 +13,29 @@ import '@telegram-apps/telegram-ui/dist/styles.css';
 import '@app/styles/global.css';
 
 postEvent('web_app_expand');
+
+
+Sentry.init({
+  dsn: "https://8821629c9179afd7378f0619233850f9@o4507655658602496.ingest.us.sentry.io/4508120836145152",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/*.tonstarsdao\.xyz\//, /^https:\/\/*.42\.works\//],
+  // Set profilesSampleRate to 1.0 to profile every transaction.
+  // Since profilesSampleRate is relative to tracesSampleRate
+  // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
+  // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
+  // results in 25% of transactions being profiled (0.5*0.5=0.25)
+  profilesSampleRate: 1.0,
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
